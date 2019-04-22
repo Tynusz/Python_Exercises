76d2283f527844fc6959ec559c3950a4c87e8fea
Janusz <49869472+Tynusz@users.noreply.github.com>
2019-04-22 16:05:08 +0200

    Exercise 1

--- cwi_1.py
+++ cwi_1.py
@@ -9,7 +9,6 @@ for i in range(0,index):
     age.append(int(input("You'r age:")))
     YEAR = _datetime.date.today().year #this will find out what year it is
     year100.append((YEAR-age[i])+100) #this will calculate year for every name
-
 #this for statement will print all names with years
 for i in range(0,index):
     print(name[i],"You will turn 100 years old in",year100[i])
